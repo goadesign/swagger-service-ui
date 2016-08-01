fd2278e172df0e7a51335717adcc6fcfc38f2388
Raphael Simon <simon.raphael@gmail.com>
2016-07-31 18:15:03 -0700

    Update pointer to swagger service so it uses appengine flexible backend

--- src/main/javascript/view/HeaderView.js
+++ src/main/javascript/view/HeaderView.js
@@ -13,10 +13,10 @@ SwaggerUi.Views.HeaderView = Backbone.View.extend({
       trigger = false;
     }
     if (!url) {
-      url = 'http://api.swagger.goa.design:8080/swagger/spec/github.com/goadesign/goa-cellar/design';
+      url = 'http://api.swagger.goa.design/swagger/spec/github.com/goadesign/goa-cellar/design';
     }
 
-    var val = url.replace(/^http:\/\/api.swagger.goa.design:8080\/swagger\/spec\/github.com\//, '');
+    var val = url.replace(/^http:\/\/api.swagger.goa.design\/swagger\/spec\/github.com\//, '');
     $('#input_baseUrl').val(decodeURIComponent(val));
 
     if (trigger) {
